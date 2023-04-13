48542f03f6ec137360fa0a2970467592d993de81
havertzz04 <havertzz04@gmail.com>
2023-04-13 09:00:18 -0500

    feat: :lipstick: Add style
    Add color and some margin values in order to imrpove the wbepage aspect

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 class lapiz {
-    #marca
+    #Dmarca
     constructor({ color = "rgb(255, 255, 0)", dimension = "20", borrador = "true", material = "madera", marca = "mongol" }) {
         this.color = color;
         this.dimension = dimension;
         this.borrador = borrador;
         this.material = material;
-        this.#marca = marca;
-    }
-}
+        this.#Dmarca = marca;
+    };
+};
 
 let obj = new lapiz({});
 let myForm = document.querySelector("#myForm");
 let tableBody = document.querySelector("#tableBody");
 
-//Estoy actualizando la clase despues de llamarla, hay que modificar eso, para actualizarla antes de llamarla.
 function updateObj(e) {
     obj.color = e.get("color");
     obj.dimension = e.get("dimension");
     obj.marca = e.get("marca");
     obj.borrador = e.get("borrador");
     obj.material = e.get("material");
-}
+};
 
 myForm.addEventListener("submit", function (e) {
     e.preventDefault(); 
@@ -39,4 +38,4 @@ myForm.addEventListener("submit", function (e) {
     colorCell.style.backgroundColor = obj.color;
 });
 
-//console.log(obj);
+console.log(obj);
